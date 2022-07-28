3a02545dfa09e0ffd09ffe12682c805bd3ebb861
Glitch (marred-rowan-dingo) <none>
2022-07-28 23:57:54 +0000

    💼😽 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,104 @@
+/**
+ * This is the main Node.js server script for your project
+ * Check out the two endpoints this back-end API provides in fastify.get and fastify.post below
+ */
+
+const path = require("path");
+const axios = require('axios')
+
+// Require the fastify framework and instantiate it
+const fastify = require("fastify")({
+  // Set this to true for detailed logging:
+  logger: false,
+});
+
+// ADD FAVORITES ARRAY VARIABLE FROM TODO HERE
+
+// Setup our static files
+fastify.register(require("@fastify/static"), {
+  root: path.join(__dirname, "public"),
+  prefix: "/", // optional: default '/'
+});
+
+// Formbody lets us parse incoming forms
+fastify.register(require("@fastify/formbody"));
+
+// View is a templating manager for fastify
+fastify.register(require("@fastify/view"), {
+  engine: {
+    handlebars: require("handlebars"),
+  },
+});
+
+// Load and parse SEO data
+const seo = require("./src/seo.json");
+if (seo.url === "glitch-default") {
+  seo.url = `https://${process.env.PROJECT_DOMAIN}.glitch.me`;
+}
+
+/**
+ * Our home page route
+ *
+ * Returns src/pages/index.hbs with data built into it
+ */
+fastify.get("/", function (request, reply) {
+  // The Handlebars code will be able to access the parameter values and build them into the page
+  console.log(JSON.stringify(request.body))
+  let resp = {
+  "fulfillmentMessages": [
+    {
+      "text": {
+        "text": [
+          "fogo"
+        ]
+      }
+    }
+  ]
+}
+  return JSON.stringify(resp);
+});
+
+/**
+ * Our POST route to handle and react to form submissions
+ *
+ * Accepts body data indicating the user choice
+ */
+fastify.post("/", async function (request, reply) {
+  // The Handlebars code will be able to access the parameter values and build them into the page
+  let url = `https://pokeapi.co/api/v2/pokemon/${request.body.queryResult.parameters.pokemon_name.toLowerCase()}`;
+  console.log(url)
+  
+  let types = await axios
+  .get(url)
+  .then(res => {
+    return res.data.types.map(x => x.type.name)
+  })
+  .catch(error => {
+    console.error(error);
+  });
+  
+  console.log(types)
+  let resp = {
+  "fulfillmentMessages": [
+    {
+      "text": {
+        "text": [types.join(' and ')]
+      }
+    }
+  ]
+}
+  return JSON.stringify(resp);
+});
+
+// Run the server and report out to the logs
+fastify.listen(
+  { port: process.env.PORT, host: "0.0.0.0" },
+  function (err, address) {
+    if (err) {
+      fastify.log.error(err);
+      process.exit(1);
+    }
+    console.log(`Your app is listening on ${address}`);
+    fastify.log.info(`server listening on ${address}`);
+  }
+);

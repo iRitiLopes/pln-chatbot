e57974b7a911ab7891b8a4c6df4348128dbae388
Richard Lopes <richard.lopes1995@gmail.com>
2022-07-28 22:19:32 -0300

    Repository pokemon

--- src/repositories/pokemon.js
+++ src/repositories/pokemon.js
@@ -1,25 +1,19 @@
 const axios = require('axios')
+const Pokedex = require('pokedex-promise-v2')
 
 class PokemonRepository {
     constructor() {
-        this.url = "https://pokeapi.co/api/v2/pokemon/"
+        this.pokedex = new Pokedex()
     }
 
 
     async pokemonType(pokemonName) {
-        let types = await axios
-            .get(this.url + `${pokemonName}`)
-            .then(res => {
-                return res.data.types.map(x => x.type.name)
-            })
-            .catch(error => {
-                console.error(error);
-            });
+        let types = await this.pokedex.getTypeByName(pokemonName);
         return types;
     }
 
     async pokemonMoves(pokemonName) {
-        
+
     }
 }
 
